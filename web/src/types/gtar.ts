75a3c6450692f86be75d08d38f5aeee264e007e9
ozzzmond <jlopez3rd@gmail.com>
2026-09-11 21:50:09 +0800

    release(web): web v1.1.75

--- web/src/types/gtar.ts
+++ web/src/types/gtar.ts
@@ -2,7 +2,7 @@
  * GTAR Android Room Entity, Setlist & Stage Line Type Definitions (v1.1.62 compatible)
  */
 
-export const GTAR_APP_VERSION = '1.1.62'
+export const GTAR_APP_VERSION = '1.1.75'
 export const GTAR_DEV_VERSION = '1.0.62-dev.13'
 export const GTAR_SETLIST_VERSION = 1
 export const GTAR_SETLIST_TYPE = 'GTAR_SETLIST'
