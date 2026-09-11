e0f73de8a4c862bbbab9fcc73169a28c176b83b9
ozzzmond <jlopez3rd@gmail.com>
2026-09-11 23:20:03 +0800

    chore(app): start app v1.0.72-dev.1 cycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 74
-        versionName = "app v1.1.72"
+        versionName = "app v1.0.72"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -104,7 +104,7 @@ android {
         }
         debug {
             applicationIdSuffix = ".debug"
-            versionNameSuffix = ""
+            versionNameSuffix = "-dev.1"
             manifestPlaceholders["appName"] = "GTAR-Dev"
             signingConfig = signingConfigs.getByName("debug")
         }
