d47836b5ec97504f4b7d2d3178e57a58f34a628e
ozzzmond <jlopez3rd@gmail.com>
2026-09-11 21:50:09 +0800

    chore(web): start web v1.0.75-dev.1

--- web/src/types/gtar.ts
+++ web/src/types/gtar.ts
@@ -3,7 +3,7 @@
  */
 
 export const GTAR_APP_VERSION = '1.1.75'
-export const GTAR_DEV_VERSION = '1.0.62-dev.13'
+export const GTAR_DEV_VERSION = '1.0.75-dev.1'
 export const GTAR_SETLIST_VERSION = 1
 export const GTAR_SETLIST_TYPE = 'GTAR_SETLIST'
 
