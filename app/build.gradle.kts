fd81f3dd24c5c3cfd328bfb36b1c8c7f2daffe21
ozzzmond <jlopez3rd@gmail.com>
2026-09-11 16:40:18 +0800

    chore(release): bump app to app v1.0.62-dev.7 (code 68)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.joel.gta"
         minSdk = 26
         targetSdk = 35
-        versionCode = 67
+        versionCode = 68
         versionName = "app v1.0.62"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -104,7 +104,7 @@ android {
         }
         debug {
             applicationIdSuffix = ".debug"
-            versionNameSuffix = "-dev.6"
+            versionNameSuffix = "-dev.7"
             manifestPlaceholders["appName"] = "GTAR-Dev"
             signingConfig = signingConfigs.getByName("debug")
         }
